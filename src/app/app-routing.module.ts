0c91eeaac503a595dc0cb182d37d1fbf0130f5b7
Simon Oulevay <simon.oulevay@gmail.com>
2017-07-17 17:03:23 +0200

    Remove bugfix for angular issue 13530 which was fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,28 +16,11 @@ import { EditExcursionZonesStepComponent } from './edit-excursion-page/edit-excu
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
     component: HomePageComponent
   },
-  /**
-   * The extra "home" route is required due to a bug in the router.
-   * When a CanActivate guard denies access to the user and navigates to "/",
-   * the home component is not rendered. Redirecting to "/home" instead,
-   * and having that path redirect to "/", solves the problem for now.
-   *
-   * There is a pull request to fix the bug already, but it has not been released yet.
-   *
-   * https://github.com/angular/angular/issues/13530
-   * https://github.com/angular/angular/pull/13929
-   */
   {
-    path: 'home',
-    redirectTo: '/'
-  },
-  {
-    path: 'excursions',
-    component: ExcursionsPageComponent,
-    canActivate: [ CanAccessPage ]
+    path: 'register',
+    component: RegistrationPageComponent
   },
   {
     path: 'excursions/new',
