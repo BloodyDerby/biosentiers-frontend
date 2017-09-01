9e20eafd617925fd9d0450a36009d1318e7a1bd3
Simon Oulevay <simon.oulevay@gmail.com>
2017-09-01 14:50:45 +0200

    Pretty bytes pipe

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,11 +2,16 @@ import { CommonModule as AngularCommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ErrorsModule } from '../errors';
+import { PrettyBytesPipe } from './pretty-bytes.pipe';
 
 @NgModule({
+  declarations: [
+    PrettyBytesPipe
+  ],
   exports: [
     AngularCommonModule,
-    ErrorsModule
+    ErrorsModule,
+    PrettyBytesPipe
   ]
 })
 export class CommonModule { }
